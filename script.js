1cbda4804fa8cca49f94c0d60622f5a71d293d42
Kiki-Ananda30 <m.riesqiananda@gmail.com>
2025-01-16 13:56:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hello, world!");
+
+const endpoint = "https://reqres.in/api/users";
+
+fetch(endpoint, {
+  method: "post",
+  headers: {
+    "content-type": "application/json",
+  },
+  body: JSON.stringify({
+    email: "johnDoe@gmail.com",
+    firstName: "John",
+    lastName: "Doe",
+    avatar:
+      "https://media.istockphoto.com/id/637234116/id/foto/melakukannya-untuk-cinta-kesuksesan.webp?s=2048x2048&w=is&k=20&c=8YeSi_YX-rRW4pUyZg7P9OkW9rs5i9xsNKxfwcdiyyY=",
+  }),
+})
+  .then((result) => result.json())
+  .then((data) => console.log(data));
